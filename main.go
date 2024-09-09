be81699e47340e4f7963b01a16bc35308b920e1b
sakis <papatzelosthanashs@gmail.com>
2024-09-09 11:57:52 +0300

    updated to use service db

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ var db *sql.DB
 func main() {
 	var err error
 	// Open a database connection
-	db, err := sql.Open("mysql", "root:example@tcp(service-db.jenkins.svc.cluster.local:3306)/test_db")
+	db, err := sql.Open("mysql", "root:example@tcp(10.108.5.31:3306)/test_db")
 	if err != nil {
 		log.Fatal(err)
 	}
